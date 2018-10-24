34c8704a37d7c6f54cd5cd8e11eccbb3b829dd00
Peiliang Zou <peiliangz@gmail.com>
2018-10-24 10:09:23 -0400

    add go concurrency examples

---
+++ onev2.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+  "fmt"
+)
+
+func main() {
+  fmt.Println("start")
+  go doSomething()
+  fmt.Println("end")
+}
+
+func doSomething() {
+  fmt.Println("do something")
+}
---
+++ times.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	for n:=2; n <= 12; n++ {
+		timestable(n)
+	}	
+}
+
+func timestable(x int) {
+	for i:=1; i <= 12; i++{
+		fmt.Printf("%d x %d = %d\n", i, x, x*i)
+		time.Sleep(100*time.Millisecond)
+	}
+}
+	
